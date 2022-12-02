06d2ddd68e75cc8ed34b7d5be2bff89a9221db38
Alexa Semyonova <semeenowa.alexandra@yandex.ru>
2022-12-02 15:00:31 +0300

    'slider'

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const buttonMoreInformation = document.querySelector('.button-brend__more')
 const brendsList = document.querySelector('.brends-list')
 
+
 buttonMoreInformation.addEventListener('click', function(){
   brendsList.classList.toggle('brends-list--full')
   buttonMoreInformation.classList.toggle('button-brend__more-rotate')
--- swiper.js
+++ swiper.js
@@ -1,13 +1,44 @@
-const swiper = new Swiper('.swiper', {
-  direction: 'horizontal',
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
+const slider = document.querySelector('.swiper');
+const swiperWrapper = document.querySelector('.swiper-wrapper')
 
-  spaceBetween: 16,
-  centerSlides: true,
-  slidesPerView: 'auto',
-  slidesOffsetAfter: 40,
-});
+let swiper;
+function mobileSlider() {
+	if (window.innerWidth < 768 && slider.dataset.mobile == 'false') {
+    swiper = new Swiper(slider, {
+      direction: 'horizontal',
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    
+      spaceBetween: 16,
+      centerSlides: true,
+      slidesPerView: 'auto',
+      slidesOffsetAfter: 40,
+      slidesOffsetBefore: 20
+    });
+    console.log('mobile')
+    slider.dataset.mobile = 'true'
+   
+  }
 
+if (window.innerWidth >= 768) {
+
+  console.log(window.innerWidth)
+  slider.dataset.mobile = 'false'
+  if (slider.classList.contains('swiper-container-initialized')) {
+    swiper.destroy();
+  }
+  swiperWrapper.classList.remove('swiper-wrapper')
+  swiperWrapper.classList.add('brends-list')
+}
+}
+
+mobileSlider()
+
+
+window.addEventListener('resize', function(){
+
+	mobileSlider();
+
+})
